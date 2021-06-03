12c7975ce14d10be18eeb3fb1c1e0ed805123271
5ubh4m <subham.sanu99@gmail.com>
2021-06-04 02:02:24 +0530

    Add support of external api for random questions

---
+++ randomQuestion.js
@@ -0,0 +1,56 @@
+const axios = require('axios');
+
+async function getQuestions(numberOfQuestions, category, level, type) {
+  try {
+    const response = await axios.get(`https://opentdb.com/api.php?amount=${numberOfQuestions}&category=${category}&difficulty=${level}&type=${type}`);
+    console.log(response.data);
+    console.log(JSON.stringify(response.data));
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+/*
+numberOfQuestions: An interger for how many question you want to return
+
+category:
+  9. General Knowledge
+  10. Entertainment: Books
+  11. Entertainment: Film
+  12. Entertainment: Music
+  13. Entertainment: Musicals & Theatres
+  14. Entertainment: Television
+  15. Entertainment: Video Games
+  16. Entertainment: Board Games
+  17. Science & Nature
+  18. Science: Computers
+  19. Science: Mathematics
+  20. Mythology
+  21. Sports
+  22. Geography
+  23. History
+  24. Politics
+  25. Art
+  26. Celebrities
+  27. Animals
+  28. Vehicles
+  29. Entertainment: Comics
+  30. Science: Gadgets
+  31. Entertainment: Japanese Anime & Manga
+  32. Entertainment: Cartoon & Animations
+
+level: easy, medium, hard
+
+type: boolean, multiple
+
+*/
+
+let numberOfQuestions, category, level, type;
+
+numberOfQuestions = 3;
+category = 31;
+level= "easy";
+type = "multiple"
+
+
+getQuestions(numberOfQuestions, category, level, type);
