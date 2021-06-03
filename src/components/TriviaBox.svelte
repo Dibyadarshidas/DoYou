27fa93ac51b6eae30b74632a51a711995450fbad
Dibyadarshidas <ddas270@gmail.com>
2021-06-03 13:48:09 +0530

    INITIAL COMMIT

---
+++ src/components/TriviaBox.svelte
@@ -0,0 +1,45 @@
+<script>
+   
+</script>
+
+<section>
+<div class="trivia-box" >
+<div class="trivia-head">
+    
+    <h4>Some Trivia Question ?</h4>
+    <div>
+        <i class="fas fa-stopwatch"></i>
+        
+        <span>25s</span>
+    </div>
+    
+</div>
+</div>
+</section>
+
+<style>
+    section{
+        height: 80vh;
+        display: grid;
+        place-items: center;
+    }
+.trivia-box{
+    width: 60%;
+    height: 60%;
+    border: 4px solid #7b1fd6;
+    border-radius: 8px;
+}
+.trivia-head{
+    display : flex;
+    justify-content: space-between;
+    padding: 20px;
+    background-color: #7b1fd6;
+    color:aliceblue;
+}
+.trivia-head i{
+    font-size: 2.5rem;
+}
+.trivia-box h4{
+    font-size: 2rem;
+}
+</style>
---
+++ src/constants/Header.svelte
@@ -0,0 +1,19 @@
+
+    <header>
+        <h3>Trivia App</h3>
+    </header>
+
+
+<style>
+header{
+    text-align: center;
+    padding: 20px;
+    background-color:#9966cc;
+    box-shadow: 0 4px 2px -2px rgba(0,0,0,.2);
+}
+h3{
+    color:aliceblue;
+    font-size: 3em;
+}
+
+</style>
